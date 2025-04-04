d12881bf17ecec66a4c3268cefff48f81815f4d7
Tasharita <tasharita.neema@gmail.com>
2025-04-04 13:55:12 +0300

    List Comprehension

--- main.py
+++ main.py
@@ -22,6 +22,13 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="What's another state's name").title()
 
     if answer_state == "Exit":
+        # The list of unknown states added to a .csv file
+        missing_states = [state for state in all_states if state not in guessed_states]
+        data_dict = {
+            "Unknown states": missing_states
+        }
+        new_data = pandas.DataFrame(data_dict)
+        new_data.to_csv("learn.csv")
         break
 
     if answer_state in all_states:
@@ -33,18 +40,7 @@ while len(guessed_states) < 50:
         t.goto(state_data.x.item(), state_data.y.item())
         t.write(answer_state)  # or (state_data.state.item())
 
-missing_states = []
-for state in all_states:
-    if state not in guessed_states:
-        missing_states.append(state)
 
-data_dict = {
-    "Unknown states" : missing_states
-}
-
-new_data = pandas.DataFrame(data_dict)
-
-new_data.to_csv("learn.csv")
 
 
 
